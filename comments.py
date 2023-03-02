6e4778717b04c59b8ede8fd5275343f15a867c26
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2023-03-02 15:06:54 +0600

    python simple basic maths

---
+++ comments.py
@@ -0,0 +1,42 @@
+# this is a comment
+
+from math import remainder
+from tokenize import Exponent
+
+
+# a = input()
+
+
+# Python Aritmetic Operators
+#  + == sum
+#  - == minus 
+#  * == multiplication
+#  / == vaag 
+#  % == remainder
+ 
+# ** Exponent === power
+# // Floor division
+
+a = 15
+b = 2
+# print(a+b);
+# print(a-b);
+# print(a*b);
+# print(a/b);
+# print(a//b)
+
+a = 2 
+b = 3
+
+print(a**b)
+
+#boolean value
+
+# True , False
+print(5>3)
+
+print(10 == (15-5))
+print(10>=10)
+
+
+print(10 != 1);
--- tomato.py
+++ tomato.py
@@ -2,7 +2,15 @@ from traceback import print_tb
 
 
 user_name = input('Please enter your name :')
-print(user_name)
+age = input("Enter your age : ")
+
+print(user_name)    
+print(age)
+
+
+#note : 
+
+# input() function === always take input as a string format
 
 
 
