bf945fcedbd09295f97d9b5dbda3829ab2156b03
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2023-03-02 16:04:41 +0600

    type casting

---
+++ typeCasting.py
@@ -0,0 +1,10 @@
+# age = float(input("Please enter your age "))
+
+# if age >= 18 :
+#     print('you are an adult')
+# else:
+#     print('you are an teen')    
+
+# print(type(age))    
+
+# note :  simply ekta int  , float habi jabi lagale e hoeye jabe r kichu lagbe na
