8185f25bb3d844f31134d2e90921121dba909127
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2023-03-02 15:45:21 +0600

    logicalOperator in python

---
+++ conditionalStateMent.py
@@ -0,0 +1,53 @@
+# from operator import truediv
+
+
+# rain = True
+
+# if rain : 
+#     print('dont go to school')
+# else:
+#     print('go to schoool')    
+
+
+
+
+
+
+from distutils.command.build_scripts import first_line_re
+from time import process_time_ns
+
+
+# rain = True
+
+# if rain ==True:
+#     print('Ami school e jabo')
+# else:
+#     print('ami scholl e a jab na')    
+
+
+# money in the pocket
+# 
+#     
+
+my_money = 4
+
+singaraPrice = 10
+friedRice = 245
+tomato = 14
+
+
+if my_money >= friedRice : 
+    print('friedRice khabo')
+elif my_money >= singaraPrice:
+    print('singara chibabo')   
+else:
+    print('tomato kheye bari jabo')   
+
+
+
+age = 17
+
+if age>=18 : 
+    print('You are elegible for a driving license ')
+else: 
+    print('Try again when you are 18')    
---
+++ logicalOperator.py
@@ -0,0 +1,9 @@
+# print((10-4==6) and (10-2==7))
+# print((10-4==6) and (10-2==8))
+# print((10-4==6) or (10-4==8))
+# print(not (10-4==6) or not (10-4==6))
+
+
+# not returns everything as opposite,, False turn to true  
+
+# and true turns to false
